de9c4dd2e7ba13a3519209e523a2a609890a72e6
Muhammad Genta Kurniawan <80944797+gentakurniawann@users.noreply.github.com>
2021-10-17 13:38:54 +0700

    Delete koneksi.php

--- koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $serverName = "localhost";
-    $userName = "root";
-    $password = "";
-    $database="buku";
-
-    //buat koneksi
-    $koneksi = mysqli_connect ($serverName, $userName, $password,$database);
-?>
